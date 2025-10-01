e7891929f3522b72155d6ab0faf72566965e5fb4
hs <hs>
2025-10-01 16:18:15 +0900

    feat: Threshold 도메인 클래스 추가

--- src/main/java/com/elim/server/gas_monitoring/domain/sensor/alarm/ThresholdSetting.java
+++ src/main/java/com/elim/server/gas_monitoring/domain/sensor/alarm/ThresholdSetting.java
@@ -2,9 +2,7 @@ package com.elim.server.gas_monitoring.domain.sensor.alarm;
 
 import com.elim.server.gas_monitoring.domain.common.entity.SoftDeletableEntity;
 import com.elim.server.gas_monitoring.infra.SnowflakeId;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +11,13 @@ import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
+@Table(
+        name = "threshold_setting",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_threshold_setting_model_serial_number",
+                columnNames = {"model", "serial_number"})
+        }
+)
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -27,7 +32,7 @@ public class ThresholdSetting extends SoftDeletableEntity {
     private String port; // 포트 (예: COM7)
 
     @Column(nullable = false, length = 30)
-    private String modelName; // 모델 명(예: UA58-KFG-U)
+    private String model; // 모델 명(예: UA58-KFG-U)
 
     @Column(nullable = false, length = 30)
     private String serialNumber; // 기기 고유 번호 (예: 25090199)
