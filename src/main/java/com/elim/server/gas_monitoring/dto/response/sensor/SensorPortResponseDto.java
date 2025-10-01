80497f902565861ace0e0ec3a413993204d80bdf
hs <hs>
2025-10-01 11:40:16 +0900

    style: 간격 수정

--- src/main/java/com/elim/server/gas_monitoring/dto/response/sensor/SensorPortResponseDto.java
+++ src/main/java/com/elim/server/gas_monitoring/dto/response/sensor/SensorPortResponseDto.java
@@ -3,7 +3,6 @@ package com.elim.server.gas_monitoring.dto.response.sensor;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
-import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -12,7 +11,6 @@ import java.util.Map;
 @AllArgsConstructor
 @Schema(description = "**전체 COM 포트 응답 DTO**")
 public class SensorPortResponseDto {
-
 
     @Schema(description = "포트명과 센서 모델명의 매핑 정보", example = "{COM3: UA58-KFG-U}")
     private Map<String, String> sensorMap;
